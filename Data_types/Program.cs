a2a96b65e5b85c246a6e12f61e0d2531a1391577
- <-@DESKTOP-5LUH4K3>
2023-11-16 19:15:56 +0300

    in progress

--- Data_types/Program.cs
+++ Data_types/Program.cs
@@ -11,8 +11,20 @@ namespace Data_types
         static void Main(string[] args)
         {
             Console.WriteLine($"bool: {sizeof(bool)}, values: {true} or {false}");
-            //Console.WriteLine($"char: {sizeof(char)}");
-            //Console.WriteLine($"short: {sizeof(short)} Байт памяти  и принимает значение в диапазоне: {short.MinValue} .... {short.MaxValue}");
+            Console.WriteLine($"bool: {sizeof(char)}");
+            Console.WriteLine($"short: {sizeof(short)} Байт памяти  и принимает значение в диапазоне: {short.MinValue} .... {short.MaxValue}");
+            Console.WriteLine($"short: {sizeof(ushort)} Байт памяти  и принимает значение в диапазоне: {ushort.MinValue} .... {ushort.MaxValue}");
+           
+            Console.WriteLine($"short: {sizeof(float)} Байт памяти  и принимает значение в диапазоне: {float.MinValue} .... {float.MaxValue}");
+            Console.WriteLine($"short: {sizeof(double)} Байт памяти  и принимает значение в диапазоне: {double.MinValue} .... {double.MaxValue}");
+            Console.WriteLine($"short: {sizeof(decimal)} Байт памяти  и принимает значение в диапазоне: {decimal.MinValue} .... {decimal.MaxValue}");
+
+
+            Console.WriteLine('+'.GetType()); ;
+            Console.WriteLine(5.GetType()); ;
+            Console.WriteLine(5.2.GetType()); ;
+            Console.WriteLine(5.2f.GetType()); ;
+            Console.WriteLine(5.2d.GetType()); ;
         }
     }
 }
